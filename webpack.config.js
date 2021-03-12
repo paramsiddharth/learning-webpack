951d12424764b8ee7fc3895c0510e3ac2083993e
Param Siddharth <contact@paramsid.com>
2021-03-12 09:30:54 +0530

    Caching

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,25 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			title: 'Output Management'
+			title: 'Title'
 		})
 	],
 	output: {
-		filename: '[name].bundle.js',
+		filename: '[name].[contenthash].js',
 		path: path.resolve(__dirname, 'dist'),
 		clean: true
 	},
+	optimization: {
+		moduleIds: 'deterministic',
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all'
+				}
+			}
+		}
+	}
 };
